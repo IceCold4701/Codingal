25c3fd4001df0bad959fd26c04becf106e621f09
IceCold4701 <SLydiaR.IceCold@gmail.com>
2024-09-06 19:45:10 +0530

    first commit

---
+++ get the country changes.py
@@ -0,0 +1,9 @@
+SPCMcountry_code={'India':'0091',
+                  'Australia':'0025',
+                  'Nepal':'00977'}
+#search dictionary for country code of India
+print("Country code of India-")
+print(SPCMcountry_code.get('India','Not Found'))
+#search dictionary for country code of Japan
+print("Country code of Japan-")
+print(SPCMcountry_code.get('Japan','Not Found'))
