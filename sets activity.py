493a5b0060d6552f36de61407948fdaa3674b58d
IceCold4701 <SLydiaR.IceCold@gmail.com>
2024-09-13 19:42:34 +0530

    first commit

---
+++ sets activity.py
@@ -0,0 +1,9 @@
+def sym_difference(set1,set2):
+    print("\nOriginal sets:")
+    print(set1)
+    print(set2)
+    result=set1.symmetric_difference(set2)
+    print("\nSymmetrical difference of setc1-setc2:")
+    return result
+seta1=set(["green", "blue"])
+seta2=set(["blue","yellow"])
